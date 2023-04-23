b8ed58b23d842064dce976f96ea88a40c655d33f
kaloyanpeev <kaloyan.peev@mentormate.com>
2023-04-23 23:47:34 +0300

    Added SignUp menu

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import SignInForm from "./SignInPage/components/forms/SignInForm";
-import { StyledSignInPageCard } from "./SignInPage.styles";
+import {
+  StyledSignInPageCard,
+  StyledAlternateSignInDiv,
+  StyledSignUpCard,
+  StyledSignInPage,
+} from "./SignInPage.styles";
 import SignInButton from "./SignInPage/components/buttons/SignInButton";
 
 const SignInPage = () => {
   return (
-    <StyledSignInPageCard>
-      <h1>Log In</h1>
-      <SignInForm />
+    <StyledSignInPage>
+      <StyledSignInPageCard>
+        <h1>Log In</h1>
+        <SignInForm />
 
-      <span>Or sign in using</span>
+        <StyledAlternateSignInDiv>
+          <div></div>
+          <div>OR</div>
+          <div></div>
+        </StyledAlternateSignInDiv>
 
-      <SignInButton />
-    </StyledSignInPageCard>
+        <SignInButton />
+      </StyledSignInPageCard>
+      <StyledSignUpCard>
+        <div>Don't have an account?</div>
+        <a href="">Sign Up</a>
+      </StyledSignUpCard>
+    </StyledSignInPage>
   );
 };
 
