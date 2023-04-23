2e0b86539b981857a6d4dee58e36c4a45ea7b8b2
kaloyanpeev <kaloyan.peev@mentormate.com>
2023-04-23 23:48:05 +0300

    Added SignInForm styles

--- src/pages/SignInPage/components/forms/SIgnInForm.styles.ts
+++ src/pages/SignInPage/components/forms/SIgnInForm.styles.ts
@@ -16,15 +16,24 @@ export const StyledForm = styled.form`
     border: none;
     padding: 10px;
     padding-left: 0px;
-    border-bottom: 1px solid white;
-    color: ${lightModeColors.primaryBgColor};
+    border-bottom: 1px solid ${lightModeColors.fdsgray30};
+    color: ${lightModeColors.fdsprimarytext};
   }
-  
 `;
 
 export const StyledLogInButton = styled.button`
   ${Button}
   width:200px;
-  border-radius: 20px;  
+  color: ${lightModeColors.fdsgray05};
+  background: ${lightModeColors.fdsprimarytext};
+  border-radius: 20px;
   margin: 0 auto;
+  transition: all 0.1s ease-in-out !important;
+  &:disabled {
+    background: ${lightModeColors.fdsgray70};
+    cursor: default;
+  }
+  &:hover:enabled {
+    filter: drop-shadow(0px 0px 4px ${lightModeColors.fdsgray45});
+  }
 `;
